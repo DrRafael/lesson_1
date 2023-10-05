d772f6cbc51db790dd6fd21faf604b09cc9ecdbc
Rafael Tugushev <63666809+DrRafael@users.noreply.github.com>
2023-10-05 14:58:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ if word in meme_dict.keys():
 else:
     print('У нас нет такого слова, Но мы над этим работаем')
             
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+            }
+word = input("Введите непонятное слово (большими буквами!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print('У нас нет такого слова, Но мы над этим работаем')
+            
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print('У нас нет такого слова, Но мы над этим работаем')
+            
