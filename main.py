07dc5ab9f99c7e4c49604f02828ae9d9228ed672
Rafael Tugushev <63666809+DrRafael@users.noreply.github.com>
2023-11-11 03:57:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
+import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://platform.kodland.org/auth/")
+time.sleep(5)
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('У нас нет такого слова, Но мы над этим работаем')
-            
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('У нас нет такого слова, Но мы над этим работаем')
-            
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
+login = driver.find_element(By.NAME,"login")
+password = driver.find_element(By.NAME,"password")
+button = driver.find_element(By.TAG_NAME,"button")
 
+login.send_keys("lgapiev")
+time.sleep(1)
+password.send_keys("nmz6e04Z8r")
+time.sleep(1)
+button.click()
+time.sleep(1)
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('У нас нет такого слова, Но мы над этим работаем')
-            
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('У нас нет такого слова, Но мы над этим работаем')
-
-@bot.command()
-async def password(ctx):
-    await ctx.send(gen_pass(10))
-            
+driver.get("https://platform.kodland.org/ru/task_57407/")
+time.sleep(5)
+for i in range(6):
+    button_id = driver.find_element(By.ID,'sub-button')
+    time.sleep(1)
+    button_id.click()
+    time.sleep(1)
+    button_ok = driver.find_element(By.ID,'submit_task_button')
+    time.sleep(1)
+    button_ok.click()
+    time.sleep(1)
+    button_next = driver.find_element(By.ID,"next_task_button")
+    time.sleep(1)
+    button_next.click()
+    time.sleep(1)
